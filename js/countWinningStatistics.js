f0cbf7b9212b43fe744abe494573f6d6d2422ec8
Marek Gudalewicz <marek.gudalewicz@localhost>
2021-04-17 13:54:37 +0200

    Added on change reaction to nr of elements input to spinnint wheel component, added dynamic table updates, however this is not good feature, as brawser optimizes it and does not change dynamicly

--- js/countWinningStatistics.js
+++ js/countWinningStatistics.js
@@ -3,14 +3,77 @@ class WinningElementStatisticsCounter extends AbstractComponent{
         super();
 
         this.state = {
-            listOfElementsInWheel: [],
+            nrOfWheelParts: 0,
             executing: false
         }
         this._state = new Proxy(this.state, this._getStateProxyHandler())
+        this._createExecutionResultsProxy();
+        this.executionResults = {}
+    }
+
+    _validateNumberInput(target, oldVal, newVal, min, max){
+        let informNotValid = function(){
+            target.classList.add('not-valid')
+            setTimeout(() => {
+                target.classList.remove('not-valid')
+            }, 500)
+            target.innerHTML = oldVal
+        }
+        if (isNaN(parseInt(newVal))){
+            informNotValid();
+            return false;
+        } else if ((parseInt(newVal) > max) || (parseInt(newVal) < min)) {
+            informNotValid();
+            return false
+        }
+        return true
     }
 
     connectedCallback() {
+        this._rememberInputValues();
         this.shadowRoot.getElementById('executionStarterId').addEventListener('click', () => {this._state.executing = true})
+        this.shadowRoot.querySelector('.nrOfElementsInput').addEventListener('input', this._changeNrOfExecitions.bind(this))
+        this._createExecutionResultsProxy();
+        this.placeTableWithResultsAsString();
+    }
+
+    _rememberInputValues(){
+        this._state.nrOfWheelParts = this.shadowRoot.querySelector('.nrOfElementsInput').innerHTML
+    }
+
+    _createExecutionResultsProxy() {
+        let obj = {};
+        let nrOfCircleParts = this._getNrOfWheelPartsFromUserInput();
+        for (let i = 0; i < nrOfCircleParts; i++) {
+            obj[i + ''] = 0;
+        }
+        delete this.executionResults
+        this.executionResults =  new Proxy(obj, this._getExecutionResultsHandler)
+    }
+
+    _changeNrOfExecitions(e){
+        let isValid = this._validateNumberInput(this.shadowRoot.querySelector('.nrOfElementsInput'), this._state.nrOfWheelParts, e.target.innerHTML, 1, 49)
+        if (isValid) {
+            this._createExecutionResultsProxy();
+            this.placeTableWithResultsAsString();
+            this._rememberInputValues();
+        }
+    }
+
+    _getExecutionResultsHandler(){
+        return {
+            set: function(obj, key, val){
+                if (obj[key] == undefined) {
+                   
+                } else {
+                    if (obj[key] != val) {
+                        this._changeTableIndex(parseInt[key], val)
+                    }
+                }
+                obj[key] = val;
+                return true
+            }.bind(this)
+        }
     }
 
     _getStateProxyHandler() {
@@ -35,6 +98,12 @@ class WinningElementStatisticsCounter extends AbstractComponent{
         }
     }
 
+    _changeTableIndex(index, val) {
+        let targetTableBody = this.shadowRoot.querySelector('table').querySelector('tbody')
+        let targetRow = targetTableBody.querySelectorAll('tr')[index];
+        targetRow.querySelectorAll('td')[1].innerHTML = val
+    }
+
     _getStateIfUndefined(){
         if(this._state == undefined) { this._state = {} } 
         if(this._state.listOfElementsInWheel == undefined) {this._state.listOfElementsInWheel = []}
@@ -43,7 +112,7 @@ class WinningElementStatisticsCounter extends AbstractComponent{
 
     createDummySpinningWheel(){
         this.testedItem = document.createElement('test-spinning-wheel')
-        this.testedItem.setNrOfWheelItems(this.getNrOfWheelPartsFromUserInput())
+        this.testedItem.setNrOfWheelItems(this._getNrOfWheelPartsFromUserInput())
     }
 
     getTestedItem() {
@@ -52,11 +121,11 @@ class WinningElementStatisticsCounter extends AbstractComponent{
         }
         return this.testedItem
     }
-    getNrOfWheelPartsFromUserInput(){
-        return parseInt(this.shadowRoot.querySelector(".nrOfElementsInput").value)
+    _getNrOfWheelPartsFromUserInput(){
+        return parseInt(this.shadowRoot.querySelector(".nrOfElementsInput").innerHTML)
     }
-    getNrOfExecutionsFromInput() {
-        return parseInt(this.shadowRoot.querySelector(".nrOfSpinsInput").value)
+    _getNrOfExecutionsFromInput() {
+        return parseInt(this.shadowRoot.querySelector(".nrOfSpinsInput").innerHTML)
     }
     countNrOfSpinWheelEntries(){
         return this.getTestedItem().querySelectorAll('li').length;
@@ -82,12 +151,13 @@ class WinningElementStatisticsCounter extends AbstractComponent{
 
     executeTests() {
         let testedItem = this.getTestedItem();    
-        let nrOfExecutions = this.getNrOfExecutionsFromInput();
+        let nrOfExecutions = this._getNrOfExecutionsFromInput();
         let indexOfExecution = 0;
         this.initiateStates();
         this.executeSingleSpin(this.getTestedItem());
         this.addWinner = function(e){
-            this._state['listOfElementsInWheel'][parseInt(e.detail)].nrOfWins++;
+            // this._state['listOfElementsInWheel'][parseInt(e.detail)].nrOfWins++;
+            this.executionResults[e.detail]++;
             indexOfExecution++;
             this.executeSingleSpin(this.getTestedItem());
             if (indexOfExecution > nrOfExecutions) {
@@ -100,12 +170,12 @@ class WinningElementStatisticsCounter extends AbstractComponent{
     executionTheardown() {
         let testedItem = this.getTestedItem();
         testedItem.removeEventListener('spinEnded', this.addWinner);
-        this.placeTableWithResultsAsStrint();
+        this.placeTableWithResultsAsString();
         this._state.listOfElementsInWheel = [];
         delete this.testedItem;
     }
 
-    placeTableWithResultsAsStrint() {
+    placeTableWithResultsAsString() {
         this.shadowRoot.querySelector('.output-placeholder').innerHTML = this.getTableWithResultsAsString()
     }
 
@@ -114,8 +184,8 @@ class WinningElementStatisticsCounter extends AbstractComponent{
         let tHead = `
             <thead><tr><th>Index</th><th>NrOfWind</th></tr></thead>
         `
-        let listOfTrItems = this.state.listOfElementsInWheel.map((item, index) => {
-            return `<tr><td>${index}</td><td>${item.nrOfWins}</td></tr>`
+        let listOfTrItems = Object.keys(this.executionResults).map((item, index) => {
+            return `<tr><td>${item}</td><td>${this.executionResults[item]}</td></tr>`
         })
         let bodyContent = listOfTrItems.reduce((acc, element) => {return acc + element})
         let tBody = `
@@ -197,10 +267,29 @@ class WinningElementStatisticsCounter extends AbstractComponent{
                 padding: 5px;
                 color: white;                
             }
-            input{
+            .input{
                 margin-right: 3rem;
                 position: relative;
                 width: 4rem;
+                width: 5rem;
+                height: 1.3rem;
+                border: solid black thin;
+                border-radius: 5px;
+                transition: 250ms;
+            }
+            .not-valid:after{
+                position: absolute;
+                // width: 6rem;
+                height: 1.4rem;
+                line-height: 1.4rem;
+                top: 2rem;
+                content: '1 to 49';
+                background: white;
+                z-index: 10000;
+            }
+            .not-valid{
+                background-color: red;
+                transition: 250ms;
             }
             
             </style>
@@ -211,9 +300,9 @@ class WinningElementStatisticsCounter extends AbstractComponent{
                 </div>
                 <div class = "title-bar center">
                     <span>Nr of elements: </span>
-                    <input class = "nrOfElementsInput" type = "number" value = "10">
+                    <div class = "nrOfElementsInput input center" type = "number" contenteditable>10</div>
                     <span>Nr of spins: </span>
-                    <input class = "nrOfSpinsInput" type = "number" value = "10">
+                    <div class = "nrOfSpinsInput input center" type = "number" contenteditable>100</div>
                 </div>
                 <div class="output-placeholder center">
                 </div>
